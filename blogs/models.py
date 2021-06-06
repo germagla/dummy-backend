31dcd1444fa081ca61c1ac67be7d9455f3f11dc9
ggt-kun <33932280+ggt-kun@users.noreply.github.com>
2021-06-06 14:57:33 +0000

    Fix Blog Model

--- blogs/models.py
+++ blogs/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.TextField(max_length=64)
+    title = models.CharField(max_length=128)
     body = models.TextField(max_length=1024)
-    author = models.TextField(max_length=32)
+    author = models.CharField(max_length=32)
 
     def __str__(self):
         return self.title
